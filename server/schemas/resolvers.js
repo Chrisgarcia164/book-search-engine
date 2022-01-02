f0e00745da942c8d50a3b966eb97d03230c04bca
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2022-01-02 13:34:26 -0500

    created typeDefs and resolvers file

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { user, book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+      }
+    },
+  },
+  Mutation: {},
+};
