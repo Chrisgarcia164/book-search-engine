505de65a2d7308918de74b9d1910a74f449417cb
Chrisgarcia164 <91617441+Chrisgarcia164@users.noreply.github.com>
2022-01-04 22:22:17 -0500

    commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,58 @@
-const { user, book } = require("../models");
+const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
-const { authenticationError } = require("apollo-server-express");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    me: async () => {},
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = User.findOne({ _id: context.user.id }).select(
+          "-password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("You are not logged in");
+    },
   },
   Mutation: {
-    login: async () => {},
-    addUser: async () => {},
-    saveBook: async () => {},
-    removeBook: async () => {},
+    addUser: async (parent, args, contexxt) => {
+      const user = await user.create(args);
+      const token = signToken(user);
+      return { user, token };
+    },
+    login: async (parent, { email, password }, context) => {
+      const user = User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user found");
+      }
+      const correctPw = await User.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Password is incorrect");
+      }
+      const token = signToken(user);
+      return { user, token };
+    },
+    saveBook: async (parent, { profileId, book }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: profileId },
+          { $addToSet: { books: book } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw new AuthenticationError("You are not logged in");
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: _id },
+          { $pull: { savedBooks: { bookId: savedBooks.bookId } } },
+          { new: true, runValidators: true }
+        );
+        return { updatedUser };
+      }
+      throw new AuthenticationError("You are not logged in");
+    },
   },
 };
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,41 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type Book {},
-type User {},
-type Query {},
-type Auth {},
-type Mutation {}
-
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: ID!
+    link: String
+    image: String
+    title: String!
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+  type Query {
+    me: User
+  }
+  input BookInput {
+    authors: [String]
+    description: String
+    bookId: ID!
+    image: String
+    link: String
+    title: String!
+  }
+  type Auth {
+    toke: ID!
+    user: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 module.exports = typeDefs;
